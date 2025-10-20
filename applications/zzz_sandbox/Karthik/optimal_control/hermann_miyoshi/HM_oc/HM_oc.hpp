f266c01b973a0b6f558cca5721a96d131782ad97
Karthik Vasudeva <kvasudev@dozza.ttu.edu>
2025-10-19 21:53:52 -0500

    converging HM_oc; need to perform u-ud

--- applications/zzz_sandbox/Karthik/optimal_control/hermann_miyoshi/HM_oc/HM_oc.hpp
+++ applications/zzz_sandbox/Karthik/optimal_control/hermann_miyoshi/HM_oc/HM_oc.hpp
@@ -1060,7 +1060,7 @@ static void AssembleBilaplaceProblem_AD(MultiLevelProblem& ml_prob) {
   KK->zero(); // Set to zero all the entries of the Global Matrix
 
 
-double alpha = .00001 ;
+double alpha = .00000001 ;
 double nu =  0. /* Poisson ratio value */;
 
 
@@ -1335,7 +1335,7 @@ double nu =  0. /* Poisson ratio value */;
      aRessxx[i] += (Bxxu + M_sxx ) * weight;  // B*W + ν1*C1*S1 + ν1*C2*S2 = -ν2*F
      aRessxy[i] += (Bxyu + M_sxy ) * weight;  // C1^T*W + M*S1 = 0
      aRessyy[i] += (Byyu + M_syy ) * weight;  // C2^T*W + M*S2 = 0
-     aResud[i] += (M_u + Bxxd + Bxyd + Byyd + udr_term) * weight;  // M*W + B^T*U = 0
+     aResud[i] += (M_u + Bxxd + Bxyd + Byyd - udr_term) * weight;  // M*W + B^T*U = 0
      aRessxxd[i] += (Bxxud + M_sxxd) * weight;  // B*W + ν1*C1*S1 + ν1*C2*S2 = -ν2*F
      aRessxyd[i] += (Bxyud + M_sxyd) * weight;  // C1^T*W + M*S1 = 0
      aRessyyd[i] += (Byyud + M_syyd ) * weight;  // C2^T*W + M*S2 = 0
